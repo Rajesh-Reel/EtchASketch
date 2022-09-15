befc8fe37f76642ba6d656c730a208411aee0f38
Rajesh-Reel <rajreel2@hotmail.com>
2022-09-15 22:06:28 +0100

    works horizontally and vertically now, moved logic inside function with parameter for size to be dynamic

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumn = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function chooseBoardSize(size) {
+  let board = document.querySelector(".board");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "red";
-  board.insertAdjacentElement("beforeend", square);
+  for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "red";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
+
+chooseBoardSize(5); // lets us choose the boards size via argument
